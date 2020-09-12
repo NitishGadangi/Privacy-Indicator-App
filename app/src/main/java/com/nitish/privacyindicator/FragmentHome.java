88135a2f5b8fec35a06a54a58c3ddf0e0a31358d
Nitish Gadangi <nitishgadangi@gmail.com>
2020-09-12 19:26:00 +0530

    Service Base SetUp

--- app/src/main/java/com/nitish/privacyindicator/FragmentHome.java
+++ app/src/main/java/com/nitish/privacyindicator/FragmentHome.java
@@ -1,20 +1,108 @@
 package com.nitish.privacyindicator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 public class FragmentHome extends Fragment {
 
+    //Root Views
+    private View root;
+    private View contentServiceEnabled;
+    private View contentServiceDisabled;
+
+    private SwitchCompat mainSwitch;
+
+    @Override
+    public void onResume() {
+        setMainContentLayouts();
+        super.onResume();
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home,container, false);
+        root = inflater.inflate(R.layout.fragment_home,container, false);
+
+        setUpViews();
+        setMainContentLayouts();
+        setUpListeners();
         return root;
+    }
+
+    private void setUpViews() {
+        mainSwitch = root.findViewById(R.id.main_switch);
+        contentServiceEnabled = root.findViewById(R.id.content_service_enabled);
+        contentServiceDisabled = root.findViewById(R.id.content_service_disabled);
+    }
+
+    private void setMainContentLayouts() {
+        if (isAccessibilityEnabled()){
+            mainSwitch.setChecked(true);
+            mainSwitch.setText("Enabled");
+            contentServiceEnabled.setVisibility(View.VISIBLE);
+            contentServiceDisabled.setVisibility(View.GONE);
+        }
+        else {
+            mainSwitch.setChecked(false);
+            mainSwitch.setText("Disabled");
+            contentServiceEnabled.setVisibility(View.GONE);
+            contentServiceDisabled.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setUpListeners() {
+        mainSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    public boolean isAccessibilityEnabled() {
+        String LOGTAG = "ACCESSIBILITY_ERROR";
+        int accessibilityEnabled = 0;
+        final String ACCESSIBILITY_SERVICE = "com.nitish.privacyindicator/com.nitish.privacyindicator.IndicatorService";
+        boolean accessibilityFound = false;
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(getActivity().getContentResolver(),android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            Log.d(LOGTAG, "Error finding setting, default accessibility to not found: " + e.getMessage());
+        }
+
+        TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
+
+        if (accessibilityEnabled==1) {
+
+            String settingValue = Settings.Secure.getString(getActivity().getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            Log.d(LOGTAG, "Setting: " + settingValue);
+            if (settingValue != null) {
+                TextUtils.SimpleStringSplitter splitter = mStringColonSplitter;
+                splitter.setString(settingValue);
+                while (splitter.hasNext()) {
+                    String accessabilityService = splitter.next();
+                    if (accessabilityService.equalsIgnoreCase(ACCESSIBILITY_SERVICE)){
+                        return true;
+                    }
+                }
+            }
+        }
+        else {
+            Log.d(LOGTAG, "***ACCESSIBILIY IS DISABLED***");
+        }
+        return accessibilityFound;
     }
 }
--- app/src/main/java/com/nitish/privacyindicator/IndicatorService.java
+++ app/src/main/java/com/nitish/privacyindicator/IndicatorService.java
@@ -1,14 +1,103 @@
 package com.nitish.privacyindicator;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.hardware.camera2.CameraManager;
+import android.media.AudioManager;
+import android.media.AudioRecordingConfiguration;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class IndicatorService extends AccessibilityService {
+    FrameLayout mLayout;
+    ImageView iv_cam, iv_mic;
+
+    CameraManager cameraManager;
+    AudioManager audioManager;
 
     @Override
     protected void onServiceConnected() {
+        createOverlay();
+        setUpInnerViews();
+        startCallBacks();
+    }
+
+    private void startCallBacks() {
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        cameraManager.registerAvailabilityCallback(new CameraManager.AvailabilityCallback() {
+            @Override
+            public void onCameraAvailable(String cameraId) {
+                super.onCameraAvailable(cameraId);
+                iv_cam.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCameraUnavailable(String cameraId) {
+                super.onCameraUnavailable(cameraId);
+                iv_cam.setVisibility(View.VISIBLE);
+                triggerVibration();
+            }
+        }, null);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        audioManager.registerAudioRecordingCallback(new AudioManager.AudioRecordingCallback() {
+            @Override
+            public void onRecordingConfigChanged(List<AudioRecordingConfiguration> configs) {
+                if (configs.size() > 0) {
+                    iv_mic.setVisibility(View.VISIBLE);
+                    triggerVibration();
+                }else {
+                    iv_mic.setVisibility(View.GONE);
+                }
+            }
+        }, null);
+    }
+
+    private void triggerVibration(){
 
     }
+
+    private void setUpInnerViews() {
+        iv_cam = mLayout.findViewById(R.id.iv_cam);
+        iv_mic = mLayout.findViewById(R.id.iv_mic);
+        iv_cam.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iv_cam.setVisibility(View.GONE);
+                iv_mic.setVisibility(View.GONE);
+            }
+        },1000);
+    }
+
+    private void createOverlay() {
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mLayout = new FrameLayout(this);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY;
+        lp.format = PixelFormat.TRANSLUCENT;
+        lp.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.gravity = getLayoutGravity();
+        LayoutInflater inflater = LayoutInflater.from(this);
+        inflater.inflate(R.layout.indicators_layout, mLayout);
+        wm.addView(mLayout, lp);
+    }
+
+    private int getLayoutGravity() {
+        return Gravity.TOP | Gravity.END;
+    }
+
 
     @Override
     public void onInterrupt() {
