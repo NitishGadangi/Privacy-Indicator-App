25f1090548687ecaa5bdde1a2cbe0b52876ea99b
Nitish Gadangi <nitishgadangi@gmail.com>
2020-09-12 13:40:01 +0530

    add dependences
    BottomNavigation

--- app/src/main/java/com/nitish/privacyindicator/HomeActivity.java
+++ app/src/main/java/com/nitish/privacyindicator/HomeActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
